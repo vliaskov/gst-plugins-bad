5de41cadb56cd33deb8bd5405b71ad299f72fbb4
Vasilis Liaskovitis <vliaskov@gmail.com>
2014-07-03 02:25:43 +0300

    glimagesink: Add gstnavigation interface

--- ext/gl/gstglimagesink.c
+++ ext/gl/gstglimagesink.c
@@ -84,6 +84,7 @@
 #endif
 
 #include <gst/video/videooverlay.h>
+#include <gst/video/navigation.h>
 
 #include "gstglimagesink.h"
 
@@ -146,6 +147,7 @@ static void gst_glimage_sink_video_overlay_init (GstVideoOverlayInterface *
 static void gst_glimage_sink_set_window_handle (GstVideoOverlay * overlay,
     guintptr id);
 static void gst_glimage_sink_expose (GstVideoOverlay * overlay);
+static void gst_glimage_sink_on_input_event (GstGLImageSink * gl_sink);
 
 static GstStaticPadTemplate gst_glimage_sink_template =
     GST_STATIC_PAD_TEMPLATE ("sink",
@@ -182,10 +184,36 @@ enum
 
 static guint gst_glimage_sink_signals[LAST_SIGNAL] = { 0 };
 
+static void
+gst_glimage_sink_navigation_send_event (GstNavigation * navigation, GstStructure
+    * structure)
+{
+  GstGLImageSink *sink = GST_GLIMAGE_SINK (navigation);
+  GstEvent *event = NULL;
+  GstPad *pad = NULL;
+
+  event = gst_event_new_navigation (structure);
+
+  pad = gst_pad_get_peer (GST_VIDEO_SINK_PAD (sink));
+
+  if (GST_IS_PAD (pad) && GST_IS_EVENT (event))
+    gst_pad_send_event (pad, event);
+
+  gst_object_unref (pad);
+}
+
+static void
+gst_glimage_sink_navigation_interface_init (GstNavigationInterface * iface)
+{
+  iface->send_event = gst_glimage_sink_navigation_send_event;
+}
+
 #define gst_glimage_sink_parent_class parent_class
 G_DEFINE_TYPE_WITH_CODE (GstGLImageSink, gst_glimage_sink,
     GST_TYPE_VIDEO_SINK, G_IMPLEMENT_INTERFACE (GST_TYPE_VIDEO_OVERLAY,
         gst_glimage_sink_video_overlay_init);
+    G_IMPLEMENT_INTERFACE (GST_TYPE_NAVIGATION,
+        gst_glimage_sink_navigation_interface_init);
     GST_DEBUG_CATEGORY_INIT (gst_debug_glimage_sink, "glimagesink", 0,
         "OpenGL Video Sink"));
 
@@ -284,6 +312,7 @@ gst_glimage_sink_class_init (GstGLImageSinkClass * klass)
 
   gstvideosink_class->show_frame =
       GST_DEBUG_FUNCPTR (gst_glimage_sink_show_frame);
+
 }
 
 static void
@@ -431,6 +460,9 @@ _ensure_gl_setup (GstGLImageSink * gl_sink)
     gst_gl_window_set_close_callback (window,
         GST_GL_WINDOW_CB (gst_glimage_sink_on_close),
         gst_object_ref (gl_sink), (GDestroyNotify) gst_object_unref);
+    gst_gl_window_set_input_event_callback (window,
+        GST_GL_WINDOW_CB (gst_glimage_sink_on_input_event),
+        gst_object_ref (gl_sink));
 
     gst_object_unref (window);
   }
@@ -1182,6 +1214,14 @@ gst_glimage_sink_on_close (GstGLImageSink * gl_sink)
   gst_gl_context_set_error (gl_sink->context, "Output window was closed");
 
   g_atomic_int_set (&gl_sink->to_quit, 1);
+}
+
+/* backend dispatch functions directly use the data for gstnavigation
+ * purposes, so the callback is a dummy for now
+ */
+static void
+gst_glimage_sink_on_input_event (GstGLImageSink * gl_sink)
+{
 }
 
 static gboolean
